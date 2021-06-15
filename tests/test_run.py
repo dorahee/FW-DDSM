9e325ad46a9a2dbd44684d410a5359a5ebe30686
Ubuntu <ubuntu@fwddsm.novalocal>
2021-06-15 05:03:00 +0000

    res

--- tests/test_run.py
+++ tests/test_run.py
@@ -19,7 +19,7 @@ algorithms[m_minizinc][m_after_fw] = f"{m_minizinc}_fw"
 
 # penalty_weight_range = [0, 5, 50, 500, 5000, 50000]
 # num_tasks_dependent_range = [0, 3, 5]
-num_households_range = [10]
+num_households_range = [100]
 # num_households_range = [50, 80, 100]
 penalty_weight_range = [1, 50, 100]
 # num_tasks_dependent_range = [0, 2, 4, 6, 8]
