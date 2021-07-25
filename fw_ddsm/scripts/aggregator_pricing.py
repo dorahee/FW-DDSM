9150f690ad4859ed2ed153d0459bdf5908b41743
Zeebee He <dora.shan.he@gmail.com>
2021-07-25 18:17:45 +1000

    edited step round up

--- fw_ddsm/scripts/aggregator_pricing.py
+++ fw_ddsm/scripts/aggregator_pricing.py
@@ -60,12 +60,12 @@ def find_step_size(num_iteration, pricing_method, pricing_table,
                 dd = dn - dp
                 dl = find_ge(d_levels, dp) + 0.01 if dd > 0 else find_le(d_levels, dp) - 0.01
                 step = (dl - dp) / dd
-                if roundup_tiny_step:
-                    step = ceil(step * roundup_step_digits) / roundup_step_digits
-                step = max(step, min_step_size)
             step_profile.append(step)
 
         step_size_incr = min(step_profile)
+        if roundup_tiny_step:
+            step_size_incr = ceil(step_size_incr * roundup_step_digits) / roundup_step_digits
+        step_size_incr = max(step_size_incr, min_step_size)
 
         return step_size_incr
 
