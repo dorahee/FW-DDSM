dd3291502997f0d79b741a52c6e3f59306602df9
dora.aws <dora.shan.he@gmail.com>
2021-07-25 07:52:31 +0000

    res

--- tests/size_run.py
+++ tests/size_run.py
@@ -18,7 +18,7 @@ algorithms[m_minizinc][m_after_fw] = f"{m_minizinc}_fw"
 # algorithms[m_ogsa][m_after_fw] = f"{m_ogsa}_fw"
 
 num_households_range = [1000]
-penalty_weight_range = [10]
+penalty_weight_range = [100]
 
 num_tasks_dependent_range = [3]
 num_full_flex_tasks = 0
@@ -35,7 +35,7 @@ battery_fully_charged_hour = 0
 battery_max_capacity_rate = 5000
 battery_min_capacity_rate = 0
 battery_power_rate = 5000
-battery_sizes = [2000, 4000, 6000, 8000, 10000, 0]
+battery_sizes = [0, 2000, 4000, 6000, 8000, 10000]
 # battery_sizes = [4000]
 # battery_efficiencies = [1, 0.99, 0.97, 0.95, 0.9, 0.75, 0.5]
 battery_efficiencies = [1]
@@ -58,7 +58,7 @@ min_step_size = 0.001
 roundup_tiny_step = True
 print_done = False
 print_steps = False
-print_steps = True
+# print_steps = True
 email_results = True
 
 
